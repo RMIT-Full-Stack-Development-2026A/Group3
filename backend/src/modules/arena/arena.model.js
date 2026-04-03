423f72c0032cb7a9b1570c426329e081c3b15573
TranNhatTie <trannhattien27032005@gmail.com>
2026-04-03 19:01:34 +0700

    Finalize database schema

--- backend/src/modules/arena/arena.model.js
+++ backend/src/modules/arena/arena.model.js
@@ -4,7 +4,7 @@ const gameRoomSchema = new mongoose.Schema({
     roomCode: { type: String, required: true, unique: true, index: true },
     hostId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
     guestId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', default: null },
-    sessionId: { type: mongoose.Schema.Types.ObjectId, ref: 'GameSession', default: null },
+    sessionId: { type: mongoose.Schema.Types.ObjectId, ref: 'game_session', default: null },
     status: { type: String, enum: ['WAITING', 'PLAYING', 'CLOSED'], default: 'WAITING' }
 }, { timestamps: true });
 
