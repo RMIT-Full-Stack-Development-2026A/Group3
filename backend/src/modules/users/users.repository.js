cfaf8e7ef0701c8cfbbe7aeb958dfcfefaef4420
mihquan <doleminhquan2005@gmail.com>
2026-04-07 15:10:18 +0700

    feat: find uer by email or id functions

--- backend/src/modules/users/users.repository.js
+++ backend/src/modules/users/users.repository.js
@@ -1,2 +1,20 @@
-/** users repository */
-module.exports = {};
+const { User } = require('./users.model');
+
+const usersRepository = {
+	findByUsernameOrEmail: async (identifier) => {
+		const normalizedIdentifier = identifier.trim().toLowerCase();
+
+		return User.findOne({
+			$or: [
+				{ username: normalizedIdentifier },
+				{ email: normalizedIdentifier }
+			]
+		});
+	},
+
+	findById: async (userId) => {
+		return User.findById(userId);
+	}
+};
+
+module.exports = usersRepository;
